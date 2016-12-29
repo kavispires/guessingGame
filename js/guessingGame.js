81c7f9f6fb54d5e7c2af9a3fcd709d84ecfe91f8
kavispires <kavispires@gmail.com>
2016-12-29 14:33:46 -0800

    Feat: shuffle function

--- js/guessingGame.js
+++ js/guessingGame.js
@@ -1,3 +1,19 @@
 function generateWinningNumber() {
 	return Math.floor(Math.random() * 100) + 1;
 }
+
+function shuffle(array) {
+	var l  = array.length,
+		t,
+		i;
+
+	while (l) {
+		i = Math.floor(Math.random() * l--);
+
+		t = array[l];
+		array[l] = array[i];
+		array[i] = t;
+	}
+	
+	return array;
+}
